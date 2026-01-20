6776241d8abef06ed9374ee2cc8601949eb90c75
Ricardo Fortis <rjfortisdev@gmail.com>
2026-01-20 13:03:07 -0600

    upwv2

--- upwork.sh
+++ upwork.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-URL="https://upwork-usw2-desktopapp.upwork.com/binaries/v5_8_0_35_be1a1520901c4eef/upwork_5.8.0.35_amd64.deb"
-FILE="/tmp/upwork_amd64.deb"
+#URL="https://upwork-usw2-desktopapp.upwork.com/binaries/v5_8_0_35_be1a1520901c4eef/upwork_5.8.0.35_amd64.deb"
+#FILE="/tmp/upwork_amd64.deb"
 
-wget -q --show-progress "$URL" -O "$FILE"
+#wget -q --show-progress "$URL" -O "$FILE"
+
+wget -q --show-progress "https://upwork-usw2-desktopapp.upwork.com/binaries/v5_8_0_35_be1a1520901c4eef/upwork_5.8.0.35_amd64.deb" -O /tmp/upwork_amd64.deb
 
 sudo apt update
 sudo apt install -y "$FILE"
