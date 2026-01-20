d1db05b5bde546d8e5448ae0302fad658bbcd30f
Ricardo Fortis <rjfortisdev@gmail.com>
2026-01-20 10:00:09 -0600

    init

---
+++ init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install -y \
+  xorg xinit lightdm lightdm-gtk-greeter \
+  i3-wm i3status i3lock xss-lock \
+  pipewire pipewire-audio-client-libraries pipewire-pulse pipewire-alsa wireplumber pavucontrol \
+  alacritty rofi picom feh lxappearance arandr \
+  thunar gvfs gvfs-backends udiskie thunar-archive-plugin tumbler \
+  dunst libnotify-bin \
+  firefox chromium-browser \
+  flameshot viewnior xclip \
+  brightnessctl xfce4-power-manager policykit-1-gnome \
+  network-manager xdg-utils dbus-x11 xdg-desktop-portal-gtk \
+  dex xdotool fastfetch htop curl wget rsync zip unzip p7zip-full p11-kit libvulkan1 \
+  fonts-dejavu fonts-font-awesome fonts-noto-color-emoji fonts-liberation fonts-inter
+
+systemctl --user --now enable pipewire pipewire-pulse wireplumber
+
+sudo systemctl enable lightdm
